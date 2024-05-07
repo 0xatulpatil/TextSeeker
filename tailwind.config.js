87e581f6f3fec96708b2f1d68e04b806ef45d282
Atul Patil <67235572+0xatulpatil@users.noreply.github.com>
2024-05-07 10:02:00 +0000

    refactor code and cosmetic changes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    borderWidth: {
+      1: "1px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
